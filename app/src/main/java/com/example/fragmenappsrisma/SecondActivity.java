0ce9562109d071e77502363f942aa09470eb56cf
rismaauliyah <auliyah.risma11.ra@gmail.com>
2021-04-10 21:51:03 +0700

    FragmentRisma

---
+++ app/src/main/java/com/example/fragmenappsrisma/SecondActivity.java
@@ -0,0 +1,6 @@
+package com.example.fragmenappsrisma;
+
+import android.app.Activity;
+
+public class SecondActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/fragmenappsrisma/firstfragment.java
@@ -0,0 +1,6 @@
+package com.example.fragmenappsrisma;
+
+import android.app.Fragment;
+
+public class firstfragment extends Fragment {
+}
